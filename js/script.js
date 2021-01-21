59dd24090afe3b453ff811c8a36d0a43bca7285b
Artem <73796530+Kola-art@users.noreply.github.com>
2021-01-22 01:34:55 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,14 +4,22 @@ $(document).ready(function() {
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/arr.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/arr_2.png"></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
-        ]
+        // responsive: [
+        //     {
+        //         breakpoint: 992,
+        //         settings: {
+        //             dots: true,
+        //             arrows: false
+        //         }
+        //     }
+        // ]
     });
 });
+// window.onload = function (){
+    let burger = document.getElementsByClassName("menuact");
+    let menu = document.getElementById("inactive");
+    console.log(burger);
+    burger.onclick = function(){
+            menu.style.display="block";
+    }
+// }
